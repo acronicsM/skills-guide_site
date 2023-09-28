c7a01218b32cf442a69be2df632705b85b0d3bb6
acronicsM <91975262+acronicsM@users.noreply.github.com>
2023-09-28 16:54:58 +0300

    1) Доделал детальную вакансии 2) Доделал списка навыков 3) Переделал страницу списка вакансий для отображения вакансий по фильтру навыка и запроса 4) Начал делать страницу запросов

--- skillguide/guide/urls.py
+++ skillguide/guide/urls.py
@@ -6,9 +6,7 @@ urlpatterns = [
     path('vacancies/', views.vacancies, name='vacancies'),
     path('vacancies/<int:vacancy_id>', views.vacancy, name='vacancy'),
     path('skills/', views.skills, name='skills'),
-    path('skills/<int:skill_id>', views.skill, name='skill'),
     path('querys/', views.querys, name='querys'),
-    path('query/<int:query_id>', views.query, name='query'),
     path('analysis/', views.analysis, name='analysis'),
     path('about/', views.about, name='about'),
 ]
